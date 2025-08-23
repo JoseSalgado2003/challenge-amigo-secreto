ef387c6122af084d5361dbd48c6cd7b96f15303b
JoseSalgado2003 <josemanuelsalgadomoreno28@gmail.com>
2025-08-23 19:37:49 -0400

    Implementar variable 'amigos' hago función para agregar amigos donde capturo los valores de entrada y hago su validación

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Desarrollar una aplicación que permita a los usuarios ingresar nombres de amigos en una lista para luego realizar un sorteo aleatorio y determinar quién es el "amigo secreto".
+
+// Array donde almacenaré los nombres de los amigos
+let amigos = [];
+
+// Función para agregar amigos
+function agregarAmigos() {
+    // Variable para almacenar el valor de entrada
+    let capturarValorEntrada = document.getElementById("Amigo");
+
+    // If para validar que haya escrito algo el usuario
+    if(capturarValorEntrada === '' || capturarValorEntrada === null) {
+        // Mostrar mensaje de error
+        alert('error: Por favor, inserte un nombre.');
+    } else {
+        amigos.push(capturarValorEntrada);
+    }
+}
+
